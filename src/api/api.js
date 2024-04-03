64eb2f54f8335a68469ccecfac5c2a1a9e08bf7e
owaisrasheed <owaisrasheed92@gmail.com>
2024-04-03 22:58:47 +0500

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '4d0004311df683e8442c233a8964c5c1';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
